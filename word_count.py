7ea2db98e62e6e5d78a246659151aeeafe1fc964
Diluxshan <diluxv@gmail.com>
2024-11-12 22:29:19 -0600

    word count file added

---
+++ word_count.py
@@ -0,0 +1,12 @@
+# filename = input('Enter the file name :')
+filename = "file.txt"
+parser = open(filename, 'r')
+
+total_count = 0
+for line in parser:
+	line = line.replace('"','')
+	words = line.split()
+	print(words)
+	print(len(words))
+	total_count = total_count + len(words)
+print(total_count)
