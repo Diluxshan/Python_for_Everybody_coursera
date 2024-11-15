b9b086116e813f79383fcbd9a0de88847c256c88
Diluxshan <diluxv@gmail.com>
2024-11-15 11:13:57 -0600

    try except lines added.

--- operators_expression.py
+++ operators_expression.py
@@ -4,5 +4,17 @@ b = 4
 total = a * b
 total2 = a ** b
 total3 = b % a
-
 print(total, ',', total2, ',', total3)
+
+x= -2
+if x < 2 :
+    print("ok")
+
+astr = 'Hello Bob'
+istr = 0
+try:
+    istr = int(astr)
+    quit()
+except:
+    istr = -1
+print(istr)
