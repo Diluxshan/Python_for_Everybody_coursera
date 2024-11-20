c0fbfe8abf222f62b2f6f6a4cf92049636d6fc5f
Diluxshan <diluxv@gmail.com>
2024-11-20 15:53:57 -0600

    Parmeter pass function

---
+++ parameter_pass.py
@@ -0,0 +1,9 @@
+def calval(a,b):
+    total = a + b
+    print(total)
+    return total
+
+param1= input("First Val:")
+param2= input("Second Val:")
+
+print("Total Cal :", calval(int(param1),int(param2)))
